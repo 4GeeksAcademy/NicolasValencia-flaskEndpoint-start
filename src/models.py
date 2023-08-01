63b4581034e8986849ae893032809dae97d71544
valenciaNe <132948614+valenciaNe@users.noreply.github.com>
2023-08-01 01:39:02 +0000

    cambios iniciales

--- src/models.py
+++ src/models.py
@@ -7,6 +7,7 @@ class User(db.Model):
     email = db.Column(db.String(120), unique=True, nullable=False)
     password = db.Column(db.String(80), unique=False, nullable=False)
     is_active = db.Column(db.Boolean(), unique=False, nullable=False)
+    facturas = db.relationship('Facturas', backref='parent',lazy=True)
 
     def __repr__(self):
         return '<User %r>' % self.username
@@ -15,5 +16,26 @@ class User(db.Model):
         return {
             "id": self.id,
             "email": self.email,
+            "facturas": list(map(lambda factura: factura.serialize(), self.facturas))
             # do not serialize the password, its a security breach
         }
+    
+
+class Facturas(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    platos = db.Column(db.String(120), unique=False, nullable=False)
+    mesa = db.Column(db.String(80), unique=False, nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey("user.id"), nullable=False)
+    
+
+    def __repr__(self):
+        return '<Facturas %r>' % self.id
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "platos": self.platos,
+            "mesa": self.mesa,
+            # do not serialize the password, its a security breach
+        }
+
